811932ea884a0d81cb546b07b0e5e63f69fa5fa5
root <root@localhost.localdomain>
2017-02-23 02:42:05 -0500

    Added insert

---
+++ webapp.py
@@ -0,0 +1,83 @@
+"""
+    Web app for CSEC-472 project 1
+
+    2/20/17
+    @author: Cameron Clark
+"""
+
+import flask
+import MySQLdb
+import os
+import time
+from flask import Flask, flash, render_template
+from werkzeug.utils import secure_filename
+
+UPLOAD_FOLDER = '/var/www/hmtl/uploads'
+app = Flask(__name__)
+app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
+app.config['SECRET_KEY'] ='DANK'
+
+def db_connect():
+    """
+        Connects to our SQL database
+
+        :returns db: the object for our database connection
+    """
+    db = MySQLdb.connect(host='localhost', user='root',
+                         passwd='itsvariable17', db='webapp')
+    return db
+
+
+@app.route('/', methods=['GET'])
+def index():
+    """
+        Returns the index page
+
+        :return index.html: our homepage
+    """
+    return render_template('index.html') 
+
+
+@app.route('/upload', methods=['POST'])
+def upload():
+    """
+        Handes the user 
+    """
+    if 'file' not in flask.request.files:
+        flash('No file part')
+        return render_template('index.html')
+    
+    f = flask.request.files['file']
+    if f.filename == '':
+        flash('No selected file')
+        return render_template('index.html')
+    
+    if f:
+        fname = secure_filename(f.filename)
+        f.save(os.path.join(app.config['UPLOAD_FOLDER'], fname))
+        flash('File uploaded successfully')
+    
+    return render_template('index.html')
+
+
+@app.route('/insert', methods=['POST'])
+def insert():
+    """
+        Function to handle a user inputting their name
+
+    """
+    name = flask.request.form.get('name')
+    if not name:
+        return render_template('index.html')
+
+    cur = db.cursor()
+    now = time.strftime("%x")
+    cur.execute("INSERT INTO names VALUES('%s', '%s')" % (name, now))
+    db.commit()
+
+    return render_template('index.html')
+
+
+if __name__ == '__main__':
+    db = db_connect()
+    app.run(host='0.0.0.0', port=5000)
