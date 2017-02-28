b708c8e98633312645aeda2212ed3d80edd0a6be
Doshmajhan <cpc1007@rit.edu>
2017-02-28 02:20:36 -0800

    Added change password

--- auth.c
+++ auth.c
@@ -1,8 +1,10 @@
 #include <security/pam_appl.h>
 #include <security/pam_misc.h>
+#include <security/pam_modules.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+
 
 static struct pam_conv conv = {
     misc_conv,
@@ -10,7 +12,7 @@ static struct pam_conv conv = {
 };
 
 
-int pam_creds_check(char *username){
+int pam_creds_check(){
     /*
         Checks to see if a user can be authenticated to the system
 
@@ -18,10 +20,16 @@ int pam_creds_check(char *username){
 
         :returns retval: the status of the check, 1 if true 0 if not
     */
-
+    const char *username;
     pam_handle_t *pamh=NULL;
     int retval;
+    
+    retval = pam_start("login", username, &conv, &pamh);
+    retval = pam_get_user(pamh, &username, "Username: ");
+    if(retval != PAM_SUCCESS){
 
+        return retval;
+    }
     retval = pam_start("check_user", username, &conv, &pamh);
     if(retval == PAM_SUCCESS){
         retval = pam_authenticate(pamh, 0);
@@ -32,6 +40,11 @@ int pam_creds_check(char *username){
 
     if(retval == PAM_SUCCESS){
         fprintf(stdout, "Authenticated\n");
+        fprintf(stdout, "Change password?(Y/N):");
+        int ans = getchar();
+        if(ans == 'Y'){
+            retval = pam_chauthtok(pamh, PAM_SILENT);
+        }
     }
     else{
         fprintf(stdout, "Not Authenticated\n");
@@ -46,43 +59,7 @@ int pam_creds_check(char *username){
     return (retval == PAM_SUCCESS ? 0:1);
 }
 
-
-void login(){
-    /* 
-        Prompts the user to login with their username and password, if
-        they account doesn't exist they can create it
-     */
-
-    char * username;
-    int toolong, ch;
-    size_t len = 20;
-    printf("Username: ");
-
-    if ((username = malloc(len)) == NULL) {
-        fprintf(stderr, "out of memory\n");
-        exit(1);
-    }
-    
-    if(fgets(username, len, stdin) == NULL){
-        exit(1);
-    }
-    if(username[strlen(username)-1] != '\n'){
-        toolong = 0;
-        while (((ch = getchar()) != '\n') && (ch != EOF)){
-            toolong = 1;
-        }
-        if(toolong == 1){
-            printf("Input too long, max size is 20\n");
-            exit(1);
-        }
-    }
-    username[strlen(username)-1] = '\0';
-
-    int reval = pam_creds_check(username);
-}
-
-
 int main(){
-    login();
+    pam_creds_check();
     return EXIT_SUCCESS;
 }
